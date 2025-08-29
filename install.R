3fabe350bbf53d7c62a5cedcd72372f6052adb9b
robinlovelace <rob00x@gmail.com>
2025-08-29 09:17:28 +0100

    Update with tmap

--- install.R
+++ install.R
@@ -1,1 +1,5 @@
-remotes::install_github('itsleeds/tds', ask = FALSE)
+if (file.exists("DESCRIPTION")) {
+  remotes::install_local(ask = FALSE)
+} else {
+  remotes::install_github('itsleeds/tds', ask = FALSE)
+}
