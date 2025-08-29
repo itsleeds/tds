2d8894e37142c3ac0f35d8277f4e1a1ca79181bb
robinlovelace <rob00x@gmail.com>
2025-08-29 10:15:13 +0100

    Better support for rspm on linux

--- install.R
+++ install.R
@@ -3,8 +3,16 @@
 ## See: https://packagemanager.posit.co/ and https://packagemanager.rstudio.com/
 rspm_url <- "https://packagemanager.rstudio.com/all/latest"
 options(repos = c(CRAN = rspm_url))
-# Ask R to prefer binary packages when available
-options(pkgType = "binary")
+# Prefer binaries on Windows/macOS, but on Linux R does not support type='binary'
+sysname <- tolower(Sys.info()["sysname"] %||% "")
+if (sysname %in% c("windows", "darwin")) {
+  options(pkgType = "binary")
+} else {
+  # Linux: install from RSPM; R will use source packages where binaries are not
+  # supported by the local R installation. RSPM still speeds up installs by
+  # providing prebuilt packages appropriate for many Linux targets.
+  options(pkgType = "source")
+}
 
 if (file.exists("DESCRIPTION")) {
   remotes::install_local(dependencies = TRUE, ask = FALSE)
